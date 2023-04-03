6697c9a40bb0dd22019a69761dd95ff5aa517ff2
yuanzhen <2504621340@qq.com>
2023-04-03 19:08:43 +0800

    feat:尝试获取当前路由

--- Short-Video-Platform-v1.0/src/components/common/Header.vue
+++ Short-Video-Platform-v1.0/src/components/common/Header.vue
@@ -9,10 +9,33 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-
-const activeIndex = ref('/')
+import { ref, watch } from 'vue'
+import { useRouter } from 'vue-router';
+let router = useRouter()
+let activeIndex = ref('/')
 const handleSelect = (key: string, keyPath: string[]) => {
     console.log(key, keyPath)
 }
+// //当前页面路由
+// let currentRoute = ref(router.currentRoute)
+// console.log("currentRoute", currentRoute);
+
+// //监听路由变化
+// watch(currentRoute, (path) => {
+//     console.log(path);
+//     if (path == "Home") {
+//         activeIndex.value = '/'
+//     }
+//     else if (path == "Short") {
+//         activeIndex.value = '/short'
+//     }
+//     else if (path == "Haokan") {
+//         activeIndex.value = '/haokan'
+//     }
+//     else if (path == "Haokan") {
+//         activeIndex.value = '/mini'
+//     }
+// }, {
+//     immediate: true //侦听器创建时立即监听
+// })
 </script>
